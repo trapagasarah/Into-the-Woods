89217f0a5b64a86ca4eb161979c2ba535fa61622
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-11 12:42:38 -0400

    created categories

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const categories = ['Oh Deer', 'Bear With Me', 'Hedge the Hog', 'Hey Foxy', 'Get Squirrely']
+const categories = ['Oh Deer', 'Bear With Me', 'Hedge the Hog', 'Hey Foxy', 'Get Squirrely','All Bark and No Bite']
 const ohDeer = [
     {
         value: 100,
@@ -159,3 +159,55 @@ const getSquirrely = [
         hasBeenSelected: false,
     },
 ]
+const  allBarkAndNoBite = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
+
+
+
+let drawCategories = function() {
+    let row = $('<div></div>')
+    row.addClass('row')
+    $('main').append(row)
+    
+    for (let i = 0; i < categories.length; i++) {
+        let categorySquare = $('<div></div>')
+        categorySquare.addClass('category-square')
+        categorySquare.html(categories[i])
+        row.append(categorySquare)
+    }
+}
+
+
+$(function(){
+    drawCategories()
+})
