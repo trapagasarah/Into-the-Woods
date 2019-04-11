3beee1e5f2e48de8a14af296d17c9cf7c7c16826
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-11 10:40:44 -0400

    created files

---
+++ app.js
@@ -0,0 +1,161 @@
+const categories = ['Oh Deer', 'Bear With Me', 'Hedge the Hog', 'Hey Foxy', 'Get Squirrely']
+const ohDeer = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
+const bearWithMe = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
+const hedgeTheHog = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
+const heyFoxy = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
+const getSquirrely = [
+    {
+        value: 100,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 200,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    }, 
+    {
+        value: 300,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 400,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+    {
+        value: 500,
+        answer: '.....',
+        question: '-----',
+        hasBeenSelected: false,
+    },
+]
